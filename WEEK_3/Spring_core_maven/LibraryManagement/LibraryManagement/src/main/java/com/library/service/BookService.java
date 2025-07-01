35f9f3d9b99a2278d80447b44f40be6c19f9d847
Samridhan2005 <141419157+Samridhan2005@users.noreply.github.com>
2025-07-01 22:49:08 +0530

    Updated BookService.java

--- WEEK_3/Spring_core_maven/LibraryManagement/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ WEEK_3/Spring_core_maven/LibraryManagement/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -4,12 +4,9 @@ import com.library.repository.BookRepository;
 
 public class BookService {
     private BookRepository bookRepository;
-
-    // Setter for dependency injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-
     public void addBook(String title) {
         bookRepository.saveBook(title);
     }
