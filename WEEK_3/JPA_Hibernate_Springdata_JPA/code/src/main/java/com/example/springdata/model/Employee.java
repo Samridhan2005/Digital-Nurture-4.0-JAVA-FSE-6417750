e8b22adcc3f0c1a8be4692b848d8c784b8e6e601
Samridhan2005 <samridhanselvan@gmail.com>
2025-07-08 20:32:27 +0530

    Hands-on 4 completed: JPA vs Hibernate vs Spring Data JPA

---
+++ WEEK_3/JPA_Hibernate_Springdata_JPA/code/src/main/java/com/example/springdata/model/Employee.java
@@ -0,0 +1,50 @@
+package com.example.springdata.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "department")
+    private String department;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", department=" + department + "]";
+    }
+}
