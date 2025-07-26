9d0ed4cc49fd6f34390b5c145e36e6056a32a6e2
Samridhan2005 <samridhanselvan@gmail.com>
2025-07-26 07:28:23 +0530

    handson 3 completed

---
+++ WEEK_6/handson_3/scorecalculatorapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { CalculateScore } from './Components/CalculateScore'
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Steeve"}
+      School={"DNV public school"}
+      total={284}
+      goal={3}
+      />
+
+
+    </div>
+  )
+}
+
+export default App
