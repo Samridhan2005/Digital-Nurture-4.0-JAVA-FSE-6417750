80d1937a02222bbc926648d610234a770f686643
Samridhan2005 <samridhanselvan@gmail.com>
2025-07-26 07:52:09 +0530

    handson 4 completed(implemented componentDidMount() hook and componentDidCatch() life cycle hook)

---
+++ WEEK_6/handson_4/blogapp/src/Posts.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        const posts = data.slice(0, 5).map(p => new Post(p.id, p.title, p.body));
+        this.setState({ posts });
+      })
+      .catch(error => {
+        console.error("Error fetching posts:", error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred: " + error);
+    console.log("Error info:", info);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong while loading posts.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
